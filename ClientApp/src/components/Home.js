9d65380e45092719f9f21da6257e5596daca1829
AINULISLAM-20 <ainkhan.20@gmail.com>
2023-10-12 16:44:42 +0500

    remove images link from home page

--- ClientApp/src/components/Home.js
+++ ClientApp/src/components/Home.js
@@ -1,105 +1,37 @@
-import React from 'react'
+import React from "react";
 
-
-// images
-import PhoneIcon from '../images/phone-icon.png';
-import EmailIcon from '../images/email-icon.png'
-
-import BrandLogo from '../images/logo-img.png'
-import mobileLogo from '../images/mobile-logo.png'
-
-import HeroImage from '../images/home-girls-img.png'
-
-import Mobile01 from '../images/app-mob-01.png'
-import Mobile02 from '../images/app-mob-02.png'
-import Mobile03 from '../images/app-mob-03.png'
-
-import HeadPhoneIcon from '../images/headphones-icon.png'
-
-import btn01 from '../images/btn-1.webp'
-import btn02 from '../images/btn-2.webp'
-
-import service01 from '../images/service-img1.jpg'
-import service02 from '../images/service-img2.jpg'
-import service03 from '../images/service-img3.jpg'
-import service04 from '../images/service-img4.jpg'
-
-import left from '../images/left-angle.png'
-
-import team01 from '../images/team-image1.png'
-import team02 from '../images/team-image2.png'
-import team03 from '../images/team-image3.png'
-import team04 from '../images/team-image4.png'
-
-import project01 from '../images/project-img1.jpg'
-import project02 from '../images/project-img2.jpg'
-import project03 from '../images/project-img3.jpg'
-import project04 from '../images/project-img4.jpg'
-import project05 from '../images/project-img5.jpg'
-import project06 from '../images/project-img6.jpg'
-
-import location from '../images/location-icon.png'
-import contact from '../images/contact-img.png'
-
-import planCheck from '../images/plan-check.png'
-
-import starImg from '../images/star-img.png'
-import clientImg1 from '../images/client-img1.png'
-import clientImg2 from '../images/client-img2.png'
-import clientImg3 from '../images/client-img3.png'
-
-import paperPlane from '../images/paper-plane-icon.png'
-import arrow from '../images/top-arrow.png'
-
-
-
-
-// components
-import Loader from './Loader/Loader';
-import PrimaryNav from './PrimaryNav/PrimaryNav';
-import SecondaryNav from './SecondaryNav/SecondaryNav';
-import Hero from './Hero/Hero';
-import Qualities from './Qualities/Qualities';
-import Services from './Services/Services';
-import TopProvider from './TopProvider/TopProvider';
-import OurProjects from './OurProjects/OurProjects';
-import ContactForm from './ContactForm/ContactForm';
-import PricingPlan from './PricingPlan/PricingPlan';
-import Tesstimonials from './Testimonials/Tesstimonials';
-import NewsLater from './NewsLater/NewsLater';
-import Footer from './Footer/Footer';
-import CopyRightFooter from './CopyRightFooter/CopyRightFooter';
-
+import Loader from "./Loader/Loader";
+import PrimaryNav from "./PrimaryNav/PrimaryNav";
+import SecondaryNav from "./SecondaryNav/SecondaryNav";
+import Hero from "./Hero/Hero";
+import Qualities from "./Qualities/Qualities";
+import Services from "./Services/Services";
+import TopProvider from "./TopProvider/TopProvider";
+import OurProjects from "./OurProjects/OurProjects";
+import ContactForm from "./ContactForm/ContactForm";
+import PricingPlan from "./PricingPlan/PricingPlan";
+import Tesstimonials from "./Testimonials/Tesstimonials";
+import NewsLater from "./NewsLater/NewsLater";
+import Footer from "./Footer/Footer";
+import CopyRightFooter from "./CopyRightFooter/CopyRightFooter";
 
 export function Home() {
   return (
     <>
-      <Loader/>
-      <PrimaryNav/>
-      <SecondaryNav/>
-      <Hero/>
-      <Qualities/>
-      <Services/>
-      <TopProvider/>
-      <OurProjects/>
-      <ContactForm/>
-      <PricingPlan/>
-      <Tesstimonials/>
-      <NewsLater/>
-      <Footer/>
-      <CopyRightFooter/>
-      
-   
-      
-
-
-
-
-     
-
-
-  
+      <Loader />
+      <PrimaryNav />
+      <SecondaryNav />
+      <Hero />
+      <Qualities />
+      <Services />
+      <TopProvider />
+      <OurProjects />
+      <ContactForm />
+      <PricingPlan />
+      <Tesstimonials />
+      <NewsLater />
+      <Footer />
+      <CopyRightFooter />
     </>
-
-  )
+  );
 }
