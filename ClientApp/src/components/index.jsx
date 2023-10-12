3efeba2fefe39e6e7abbeed58eac2f945be03c71
MuhammadAzeem000 <azeemk994@gmail.com>
2023-10-12 17:27:26 +0500

    add index files for each folder

---
+++ ClientApp/src/components/index.jsx
@@ -0,0 +1,17 @@
+import Loader from "./Loader/Loader";
+import PrimaryNav from "./PrimaryNav/PrimaryNav";
+import SecondaryNav from "./SecondaryNav/SecondaryNav";
+import Hero from "./Hero/Hero";
+import Qualities from "./Qualities/Qualities";
+import Services from "./Services/Services";
+import TopProvider from "./TopProvider/TopProvider";
+import OurProjects from "./OurProjects/OurProjects";
+import ContactForm from "./ContactForm/ContactForm";
+import PricingPlan from "./PricingPlan/PricingPlan";
+import Tesstimonials from "./Testimonials/Tesstimonials";
+import NewsLater from "./NewsLater/NewsLater";
+import Footer from "./Footer/Footer";
+import CopyRightFooter from "./CopyRightFooter/CopyRightFooter";
+
+
+export { Loader, PrimaryNav, SecondaryNav, Hero, Qualities, Services, TopProvider, OurProjects, ContactForm, PricingPlan, Tesstimonials, NewsLater, Footer, CopyRightFooter }
---
+++ ClientApp/src/pages/index.jsx
@@ -0,0 +1,10 @@
+import { Home } from "./Home";
+import { AboutUs } from "./AboutUs";
+import { ContactUs } from "./ContactUs";
+
+import { Login } from "./Login";
+import { PrivacyPolicy } from "./PrivacyPolicy";
+import { TermsAndConditions } from "./TermsAndConditions";
+
+
+export { Home, AboutUs, ContactUs, Login, PrivacyPolicy, TermsAndConditions }
