08bb8340557e7e5955930dfee8b30241e2405667
MuhammadAzeem000 <azeemk994@gmail.com>
2023-10-13 20:00:05 +0500

    fix css issues

--- ClientApp/src/components/NewsLater/NewsLater.jsx
+++ ClientApp/src/components/NewsLater/NewsLater.jsx
@@ -6,32 +6,26 @@ import paperPlane from '../../images/paper-plane-icon.png'
 
 export default function NewsLater() {
   return (
-    <section
-      className="w-100 float-left newsletter-con"
-      data-aos="fade-up"
-      data-aos-duration={600}
-    >
-      <div className="container">
-        <div className="newsletter-box">
-          <div className="position-relative newsletter-content">
-            <figure className="mb-0">
-              <img
-                src={paperPlane}
-                alt="paper-plane-icon"
-              />
-            </figure>
-            <div className="newsletter-text">
-              <h3>Subscribe To Our Newsletter</h3>
-              <span className="d-block">
-                Stay informed and get exclusive offers! Subscribe to our
-                newsletter for the latest updates and cleaning service promotions
-              </span>
-            </div>
+    <section className="container">
+      <div className="newsletter-box">
+        <div className="position-relative newsletter-content">
+          <figure className="mb-0">
+            <img
+              src={paperPlane}
+              alt="paper-plane-icon"
+            />
+          </figure>
+          <div className="newsletter-text">
+            <h3>Subscribe To Our Newsletter</h3>
+            <span className="d-block">
+              Stay informed and get exclusive offers! Subscribe to our
+              newsletter for the latest updates and cleaning service promotions
+            </span>
           </div>
-          <div className="news-letter-input">
-            <input type="email" placeholder="Email Address...." />
-            <button type="submit">Subscribe</button>
-          </div>
+        </div>
+        <div className="news-letter-input">
+          <input type="email" placeholder="Email Address...." />
+          <button type="submit">Subscribe</button>
         </div>
       </div>
     </section>
